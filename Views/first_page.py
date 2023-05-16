3752d37110d0e1d13fe655df9f498bc123603f25
vinay Pant <vinaypant24@gmail.com>
2023-05-16 09:06:33 +0530

    Base Files Done

---
+++ Views/first_page.py
@@ -0,0 +1,13 @@
+import tkinter as tk
+from tkinter import ttk
+from typing import Any
+import customtkinter as ctk
+
+import styles
+
+
+
+class login_page():
+
+    def __init__(self , master , wdth , height ) -> None:
+        pass
